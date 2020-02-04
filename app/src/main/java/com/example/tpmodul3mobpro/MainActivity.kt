f3542e7c7512d89e10b89a801da79f50b71ffe6d
annisanurfauziyah <annisafauziyah.af@gmail.com>
2020-02-04 17:37:50 +0700

    initial commit

---
+++ app/src/main/java/com/example/tpmodul3mobpro/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tpmodul3mobpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
